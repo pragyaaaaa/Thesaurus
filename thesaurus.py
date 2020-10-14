5ef772f9129ce4c37923c7ae66d2a6fb75b8f9e0
pragyaaaaa <pragyasabstracts@gmail.com>
2020-10-14 20:17:29 +0530

    Validation: best-match recommendation.

--- thesaurus.py
+++ thesaurus.py
@@ -1,4 +1,5 @@
 import json
+from difflib import SequenceMatcher, get_close_matches
 
 # help(json.load)
 data = json.load(open("data.json"))
@@ -8,6 +9,12 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        close_match = input("Did you mean %s ?" % get_close_matches(word, data.keys()))
+        close_match = close_match.lower()
+        if close_match == "yes" or close_match == "y":
+            word_c = input("Okay! enter the correct word: ")
+            return data[word_c]
     else:
         return "Word doesn't exist. Reconsider spell check."
 
